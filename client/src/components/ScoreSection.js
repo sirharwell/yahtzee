b1360137682bc6c52a71364d2b7f53928cffa5a6
Ian Harwell <sirharwell@gmail.com>
2018-03-27 11:58:21 -0600

    scoresection

--- client/src/components/ScoreSection.js
+++ client/src/components/ScoreSection.js
@@ -8,6 +8,43 @@ import { connect } from 'react-redux';
 import ScoreRow from './ScoreRow';
 
 class ScoreSection extends React.Component {
+
+  total = (score, label) => {
+    return (
+      <List.Item key={label}>
+        <Header floated="left">{label}</Header>
+        <Header
+          floated="right"
+          style={{ marginRight: '20px' }}
+        >
+          {score}
+        </Header>
+      </List.Item>
+    )
+  }
+
+  generateTotals = () => {
+    const { currentGame: { scores }, label } = this.props;
+    const sectionScores = [];
+    const sectionTotal = scores
+      .filter( s => s.section === label.toLowerCase() )
+      .reduce( (total, entry) => {
+        const score = entry.score || 0
+        return total + score
+      }, 0)
+
+    sectionScores.push(this.total(sectionTotal, 'Section Total'))
+
+    if (label === 'Upper') {
+      const bonus = sectionTotal >= 63 ? 35 : 0
+      sectionScores.push(this.total(bonus, 'Bonus'))
+      sectionScores.push(this.total(bonus + sectionTotal, 'Total Upper'))
+    }
+
+    return sectionScores;
+
+  }
+
   render() {
     const { label, currentGame: { scores }} = this.props
     return (
@@ -20,6 +57,7 @@ class ScoreSection extends React.Component {
                 <ScoreRow key={i} {...score} />
               )
           }
+          { this.generateTotals() }
         </List>
       </Segment>
     )
